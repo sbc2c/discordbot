6e27f9e1571d9a77167ab424832d2f22237ccd61
sbc2c <sreeharshab2010@gmail.com>
2026-05-13 23:43:30 -0500

    Integrate dotenv for environment variable management
    Added dotenv support for loading environment variables.

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import discord
 from discord.ext import commands
 import random
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-
 intents = discord.Intents.default()
 intents.message_content = True
 
