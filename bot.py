6454b7f657c860d3d69b4c04fc01ab2017ee2d97
sbc2c <sreeharshab2010@gmail.com>
2026-05-13 23:28:58 -0500

    Initialize Discord bot with ping command

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import random
+
+TOKEN = "YOUR_TOKEN_HERE"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
